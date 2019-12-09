f9d9725db388f583f19b274d89b9dd754792202a
github username <github email address>
2019-12-08 19:45:47 -0800

    python labs

---
+++ speaking-grandma.py
@@ -0,0 +1,10 @@
+def speak_to_grandma (string_phrase):
+	if string_phrase == "I LOVE YOU GRANDMA!":
+		print("I LOVE YOU TOO PUMPKIN!")
+	elif string_phrase.upper() == string_phrase:
+		print("NO, NOT SINCE 1938!")
+	else:
+		print("HUH?! SPEAK UP, SONNY!")
+
+
+speak_to_grandma("I LOVE YOU GRANDMA!")
