4361e55382a2cc0bf18b8b85d84da3b8a5539c1d
github username <github email address>
2019-12-15 21:45:35 -0800

    addition of student class and link

--- classes-books/Run.py
+++ classes-books/Run.py
@@ -1,7 +1,14 @@
 from Book import Book
+from Student import Student
 
-her_smoke = Book("Her Smoke Rose Up Forever", "James Tiptree", 0, 100)
+her_smoke = Book("Her Smoke Rose Up Forever", "James Tiptree", 40, 100)
+caves_of_steel = Book("Caves of Steel", "Isaac Asimov", 0, 100)
+katana = Student("Katana", her_smoke)
 
 print(her_smoke.completed_pages)
-her_smoke.turn_page()
+her_smoke.complete_homework()
 print(her_smoke.completed_pages)
+
+print(katana.favorite_book.title)
+katana.assign_new_favorite_book(caves_of_steel)
+print(katana.favorite_book.title)
